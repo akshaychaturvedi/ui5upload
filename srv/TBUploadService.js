931fdcc085746c4d23f9d1d73145dac18a04d443
akshaychaturvedi <akshaychaturvedi129@gmail.com>
2023-08-09 11:49:08 +0000

    i18n

--- srv/TBUploadService.js
+++ srv/TBUploadService.js
@@ -7,36 +7,6 @@ const XLSX = require('xlsx');
 
 module.exports = async function (srv) {
 
-    async function CallEntity(entity, data) {
-        // Table Declarations
-        var TrialBalanceTable = [];
-
-        for (var i = 0; i < data.length; i++) {
-            var TrialBalanceID = randomUUID();
-            var TrialBalanceEntry = {
-                "ID": TrialBalanceID,
-                "companyCode": data[i].CompanyCode,
-                "year": data[i].Year,
-                "ledger": data[i].Ledger,
-                "glaccount": data[i].GLAccount,
-                "period": data[i].Period,
-                "lc": data[i].LocalCurrency,
-                "gc": data[i].GlobalCurrency,
-                "source": data[i].Source,
-                "profitCenter": data[i].ProfitCenter,
-                "costCenter": data[i].CostCenter,
-                "businessArea": data[i].BusinessArea,
-                "division": data[i].Division,
-                "functionalArea": data[i].FunctionalArea,
-                "chartOfAccounts": data[i].ChartOfAccounts
-            };
-            TrialBalanceTable.push(TrialBalanceEntry)
-        }
-
-        return TrialBalanceTable;
-
-    };
-
     srv.on("UploadData", async (req) => {
         console.log(JSON.parse(req.data.data));
         var extract = JSON.parse(req.data.data);
@@ -72,54 +42,4 @@ module.exports = async function (srv) {
         return insertResult;
     });
 
-    srv.on('PUT', "ExcelUpload", async (req, res) => {
-        
-        var responseData;
-
-        if (req.data.excel) {
-            var buffers = [];
-            var entity = req.headers.slug;
-
-            const stream = new PassThrough();
-            req.data.excel.pipe(stream);
-
-            await new Promise((resolve, reject) => {
-
-                stream.on('data', dataChunk => {
-                    buffers.push(dataChunk);
-                });
-
-                stream.on('end', async () => {
-
-                    let data = [];
-                    var buffer = Buffer.concat(buffers);
-                    var workbook = XLSX.read(buffer, { type: 'buffer' })
-
-                    const worksheet = workbook.SheetNames[0];
-                    const sheets = workbook.SheetNames
-
-                    for (let i = 0; i < sheets.length; i++) {
-                        const temp = XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[i]])
-                        temp.forEach((res) => {
-                            data.push(res)
-                        })
-                    }
-
-                    if (data) {
-                        const responseCall = await CallEntity(entity, data);
-                        if (responseCall == -1)
-                            reject(req.error(400, JSON.stringify(data)));
-                        else {
-                            responseData = JSON.stringify(responseCall);
-
-                            resolve(req.notify({
-                                message: 'Upload Successful',
-                                status: 200
-                            }));
-                        }
-                    }
-                });
-            });
-        }
-    })
 }
